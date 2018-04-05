9573ece169a7321004252d06a5e6db29f8c40308
Hanna Lindgren <han.lind@gmail.com>
2018-04-05 13:24:08 +0200

    js/navbar

---
+++ js/navbar.js
@@ -0,0 +1,24 @@
+function renderSimple(id) {
+    var e = document.getElementById(id);
+    var timestamp = new Date().toISOString();
+    var name = e.id;
+    e.innerHTML="<h2>" + name + "</h2> " + name + " was called at " + timestamp;
+}
+
+$("a[href='#home']").on("click", function(event) {
+    var id = this.getAttribute("href", 2).replace("#","");
+    renderSimple(id);
+});
+
+$("a[href='#about']").on("click", function(event) {
+    var id = this.getAttribute("href", 2).replace("#","");
+    renderSimple(id);
+});
+
+$("a[href='#contact']").on("click", function(event) {
+    var id = this.getAttribute("href", 2).replace("#","");
+    renderSimple(id);
+});
+
+$("a[href='#home']").click();
+
