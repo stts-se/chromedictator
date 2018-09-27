24c9e5bee4fa471da6f7776b997abc726b276b8c
nikolaj <nikolaj.lindberg@stts.se>
2018-09-27 19:59:23 +0200

    stubs for persisting map as gob

--- chromedictator.go
+++ chromedictator.go
@@ -1,16 +1,63 @@
 package main
 
 import (
+	"encoding/gob"
 	"fmt"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"os"
+	"sync"
 	"time"
 )
 
 //func js(w http.ResponseWriter, r *http.Request) {
 //	http.ServeFile(w, r, "./js/dictator.js")
 //}
+
+var abbrevs = make(map[string]string)
+var mutex = &sync.RWMutex{}
+
+func mapGob2File(m map[string]string, fName string) error {
+
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	fh, err := os.Open(fName)
+	if err != nil {
+		return fmt.Errorf("mapGob2File: failed to open file: %v", err)
+	}
+	defer fh.Close()
+
+	encoder := gob.NewEncoder(fh)
+	err = encoder.Encode(m)
+	if err != nil {
+		return fmt.Errorf("mapGob2File: gob encoding failed: %v", err)
+	}
+
+	return nil
+}
+
+func gobFile2Map(fName string) (map[string]string, error) {
+
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	fh, err := os.Open(fName)
+	if err != nil {
+		return nil, fmt.Errorf("gobFile2Map: failed to open file: %v", err)
+	}
+	defer fh.Close()
+
+	decoder := gob.NewDecoder(fh)
+	m := make(map[string]string)
+	err = decoder.Decode(m)
+	if err != nil {
+		return nil, fmt.Errorf("gobFile2Map: gob decoding failed: %v", err)
+	}
+
+	return m, nil
+}
 
 func index(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "./js/index.html")
